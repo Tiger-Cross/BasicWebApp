16c535fb56970f3b76c30f190923afa78f4cfe6e
Tiger Cross <tcross123@hotmail.co.uk>
2021-01-28 17:22:31 +0000

    Add stringbuilder + more results

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,12 +3,35 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
+        StringBuilder results = new StringBuilder();
         if (query.toLowerCase().contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-                    "English poet, playwright, and actor, widely regarded as the greatest " +
-                    "writer in the English language and the world's pre-eminent dramatist.";
+            results.append(
+                    "William Shakespeare (26 April 1564 - 23 April 1616) was an\n" +
+                            "English poet, playwright, and actor, widely regarded as the greatest\n"
+                            +
+                            "writer in the English language and the world's pre-eminent dramatist. \n");
+            results.append(System.lineSeparator());
         }
-        // Process something
-        return "";
+
+        if (query.toLowerCase().contains("asimov")) {
+            results.append("Isaac Asimov (2 January 1920 - 6 April 1992) was an\n" +
+                    "American writer and professor of Biochemistry, famous for\n" +
+                    "his works of hard science fiction and popular science. \n");
+            results.append(System.lineSeparator());
+        }
+
+        if (query.toLowerCase().contains("einstein")) {
+            results.append("Albert Einstein (14 March 1879 –18 April 1955)\n"
+                    + " was a German - born theoretical physicist who\n "
+                    + "developed the theory of relativity, one of the two pillars\n"
+                    + " of modern physics(alongside quantum mechanics).");
+            results.append(System.lineSeparator());
+        }
+
+        if (results.length() == 0) {
+            return "";
+        }
+
+        return results.toString();
     }
 }
