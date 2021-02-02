55577dd0c315e19084e6df064660852f13c73cc8
James Langley <jwblangley@gmail.com>
2021-02-02 13:45:35 +0000

    Add queryWords split

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -5,6 +5,8 @@ public class QueryProcessor {
     public String process(String query) {
         StringBuilder results = new StringBuilder();
 
+        String[] queryWords = query.split(" ");
+
         if (query.toLowerCase().contains("your name")) {
             results.append("jtn");
             results.append(System.lineSeparator());
